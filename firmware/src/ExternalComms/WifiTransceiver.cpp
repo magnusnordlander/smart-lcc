4cca8c84d381f017b25d20ade3b5a40dbe563c26
Magnus Nordlander <magnus@nordlander.se>
2021-07-16 20:47:04 +0200

    WIP

--- firmware/src/ExternalComms/WifiTransceiver.cpp
+++ firmware/src/ExternalComms/WifiTransceiver.cpp
@@ -3,3 +3,35 @@
 //
 
 #include "WifiTransceiver.h"
+
+#include <mbed.h>
+
+#define WIFI_PASS "pass"
+
+using namespace std::chrono_literals;
+
+void WifiTransceiver::run() {
+    while (wifi.status() != WL_CONNECTED) {
+        printf("Attempting to connect to WiFi\n");
+        wifi.begin("IoT Transform", WIFI_PASS);
+
+        rtos::ThisThread::sleep_for(5s);
+    }
+
+    printf("Connected to WiFi\n");
+
+    while (true) {
+        printf("Pinging ping.sunet.se: \n");
+        auto pingResult = WiFi.ping("ping.sunet.se");
+
+        if (pingResult >= 0) {
+            printf("Success, RTT %d ms\n", pingResult);
+        } else {
+            printf("Failed, error %d\n", pingResult);
+        }
+
+        rtos::ThisThread::sleep_for(5s);
+    }
+}
+
+WifiTransceiver::WifiTransceiver() = default;
--- firmware/src/ExternalComms/WifiTransceiver.h
+++ firmware/src/ExternalComms/WifiTransceiver.h
@@ -5,12 +5,16 @@
 #ifndef FIRMWARE_WIFITRANSCEIVER_H
 #define FIRMWARE_WIFITRANSCEIVER_H
 
-
+#include <WiFi.h>
 
 class WifiTransceiver {
+public:
+    WifiTransceiver();
+    [[noreturn]] void run();
 
+private:
+    WiFiClass wifi;
 };
-
 
 
 #endif //FIRMWARE_WIFITRANSCEIVER_H
