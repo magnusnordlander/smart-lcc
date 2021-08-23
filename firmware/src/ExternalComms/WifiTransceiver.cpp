1c081bff674b254184ca4768e2dcace33650221f
Magnus Nordlander <magnus@nordlander.se>
2021-08-23 21:11:40 +0200

    Fix for crashes probably

--- firmware/src/ExternalComms/WifiTransceiver.cpp
+++ firmware/src/ExternalComms/WifiTransceiver.cpp
@@ -340,17 +340,28 @@ void WifiTransceiver::publish(const char *topic, uint8_t payload) {
 
 void WifiTransceiver::publish(const char *topic, float payload) {
     char floatString[FLOAT_MAX_LEN];
-/*    auto payloadInt = (int16_t)payload;
+
+    // I *think* there's something wrong with printf:ing floats. It's been causing hard faults and wrong numbers
+    // This ugly ass way of formatting floats to a string solves it.
+    auto payloadInt = (int16_t)payload;
     auto fractionsInt = (uint8_t)abs((int)((payload*100)-(float)(payloadInt*100)));
-    unsigned int len = snprintf(floatString, FLOAT_MAX_LEN, "%d.%u", payloadInt, fractionsInt);*/
-    unsigned int len = snprintf(floatString, FLOAT_MAX_LEN, "%.2f", payload);
+    unsigned int len = snprintf(floatString, FLOAT_MAX_LEN, "%d.%02u", payloadInt, fractionsInt);
+
+    //unsigned int len = snprintf(floatString, FLOAT_MAX_LEN, "%.2f", payload);
     pubSubClient.publish(topic, floatString, len);
     handleYield();
 }
 
 void WifiTransceiver::publish(const char *topic, double payload) {
     char floatString[FLOAT_MAX_LEN];
-    unsigned int len = snprintf(floatString, FLOAT_MAX_LEN, "%.2f", payload);
+
+    // I *think* there's something wrong with printf:ing floats. It's been causing hard faults and wrong numbers
+    // This ugly ass way of formatting floats to a string solves it.
+    auto payloadInt = (int16_t)payload;
+    auto fractionsInt = (uint8_t)abs((int)((payload*100)-(float)(payloadInt*100)));
+    unsigned int len = snprintf(floatString, FLOAT_MAX_LEN, "%d.%02u", payloadInt, fractionsInt);
+    
+    //unsigned int len = snprintf(floatString, FLOAT_MAX_LEN, "%.2f", payload);
     pubSubClient.publish(topic, floatString, len);
     handleYield();
 }
